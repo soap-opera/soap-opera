b41e15ac89246c5269baf3e06c9031dad89c3c2d
jg10-mastodon-social <181532694+jg10-mastodon-social@users.noreply.github.com>
2026-01-25 07:24:23 +0000

    feat: optionally make requests using bot webid

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,5 +5,6 @@ if (!process.env.PORT) throw new Error('Please specify PORT')
 const isBehindProxy = stringToBoolean(process.env.PROXY)
 const port = +process.env.PORT
 const baseUrl = process.env.BASE_URL ?? `http://localhost:${port}`
+const webId = process.env.WEB_ID
 
-export { baseUrl, isBehindProxy, port }
+export { baseUrl, isBehindProxy, port, webId }
