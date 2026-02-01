70e7830ccbd7828618d978a4cbd7634d8b420cb1
mrkvon <mrkvon@protonmail.com>
2026-02-01 16:55:11 +0100

    wip

--- src/test/helpers/pod.ts
+++ src/test/helpers/pod.ts
@@ -28,9 +28,9 @@ export const setupActor = async (person: Person, app: string) => {
 
   // make sure cached webId oidcIssuers in CSS expire before continuing
   // https://github.com/CommunitySolidServer/access-token-verifier/blob/718f7dde42df358f339e78b836d909f10df099a5/src/config/index.ts#L16
-  vi.useFakeTimers({ now: Date.now() - 121000 })
+  vi.useFakeTimers({ now: Date.now() - 121000, shouldAdvanceTime: true })
 
-  await new Promise(resolve => setTimeout(resolve, 100))
+  // await new Promise(resolve => setTimeout(resolve, 100))
 
   // save well-known
   await createWebfinger(person, actorUrl)
